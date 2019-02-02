d77fbc65ad7eba4b82ce542fff754215ea5719b3
yasuyuky <yasuyuki.ymd@gmail.com>
2019-02-02 14:16:52 +0900

    Refactor a function

--- src/lib.rs
+++ src/lib.rs
@@ -23,6 +23,37 @@ mod tests {
 
     #[test]
     fn test_term() {
+        fn handle_event(ev: &Event, term: &mut cursormatrix::Term) -> bool {
+            match ev {
+                &Event::Ctrl('C') => return false,
+                &Event::TimeOut => return false,
+                &Event::Ctrl('L') => term.cursor.clear().unwrap(),
+                &Event::Ctrl('R') => term.cursor.reload().unwrap(),
+                &Event::Ctrl('A') => term.cursor.move_home().unwrap(),
+                &Event::Ctrl('E') => term.cursor.move_end().unwrap(),
+                &Event::Arrow(Direction::Up) => term.cursor.move_up().unwrap(),
+                &Event::Arrow(Direction::Down) => term.cursor.move_down().unwrap(),
+                &Event::Arrow(Direction::Left) => term.cursor.move_left().unwrap(),
+                &Event::Arrow(Direction::Right) => term.cursor.move_right().unwrap(),
+                &Event::Ctrl('D') => term.cursor.delete_char().unwrap(),
+                &Event::Delete => term.cursor.delete_char().unwrap(),
+                &Event::BackSpace => term.cursor.backspace().unwrap(),
+                &Event::Chars(ref s) => {
+                    use unicode_normalization::UnicodeNormalization;
+                    term.cursor
+                        .print_here(&format!("{}", s.nfkc().collect::<String>()))
+                        .unwrap();
+                }
+                e => {
+                    let pos = term.cursor.get_pos();
+                    term.cursor
+                        .print_here(format!("e: {:?}, pos{:?}", e, pos).as_str())
+                        .unwrap();
+                }
+            }
+            true
+        }
+
         let mut term = match cursormatrix::Term::new() {
             Ok(term) => term,
             Err(_) => return,
@@ -59,38 +90,6 @@ mod tests {
             println!("{:?}:{:?}", k, v);
         }
         assert!(true)
-    }
-
-    #[allow(dead_code)]
-    fn handle_event(ev: &Event, term: &mut cursormatrix::Term) -> bool {
-        match ev {
-            &Event::Ctrl('C') => return false,
-            &Event::TimeOut => return false,
-            &Event::Ctrl('L') => term.cursor.clear().unwrap(),
-            &Event::Ctrl('R') => term.cursor.reload().unwrap(),
-            &Event::Ctrl('A') => term.cursor.move_home().unwrap(),
-            &Event::Ctrl('E') => term.cursor.move_end().unwrap(),
-            &Event::Arrow(Direction::Up) => term.cursor.move_up().unwrap(),
-            &Event::Arrow(Direction::Down) => term.cursor.move_down().unwrap(),
-            &Event::Arrow(Direction::Left) => term.cursor.move_left().unwrap(),
-            &Event::Arrow(Direction::Right) => term.cursor.move_right().unwrap(),
-            &Event::Ctrl('D') => term.cursor.delete_char().unwrap(),
-            &Event::Delete => term.cursor.delete_char().unwrap(),
-            &Event::BackSpace => term.cursor.backspace().unwrap(),
-            &Event::Chars(ref s) => {
-                use unicode_normalization::UnicodeNormalization;
-                term.cursor
-                    .print_here(&format!("{}", s.nfkc().collect::<String>()))
-                    .unwrap();
-            },
-            e => {
-                let pos = term.cursor.get_pos();
-                term.cursor
-                    .print_here(format!("e: {:?}, pos{:?}", e, pos).as_str())
-                    .unwrap();
-            },
-        }
-        true
     }
 
     #[allow(dead_code)]
