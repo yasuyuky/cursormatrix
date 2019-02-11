277d6fa583c59196014f5c650e5cd80dc6a79282
yasuyuky <yasuyuki.ymd@gmail.com>
2019-02-12 00:09:40 +0900

    Format

--- src/lib.rs
+++ src/lib.rs
@@ -41,13 +41,13 @@ mod tests {
                     term.cursor
                         .print_here(&format!("{}", s.nfkc().collect::<String>()))
                         .unwrap();
-                }
+                },
                 e => {
                     let pos = term.cursor.get_pos();
                     term.cursor
                         .print_here(format!("e: {:?}, pos{:?}", e, pos).as_str())
                         .unwrap();
-                }
+                },
             }
             true
         }
@@ -99,11 +99,9 @@ mod tests {
         }
         println!("strings:");
         for element in &terminfo.strings {
-            println!(
-                "strings:{}\t{:?}",
-                element.0,
-                String::from_utf8(element.1.clone()).unwrap()
-            );
+            println!("strings:{}\t{:?}",
+                     element.0,
+                     String::from_utf8(element.1.clone()).unwrap());
         }
     }
 
