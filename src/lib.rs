bdc54511bdc065a2602705d1b029d354ce189d07
yasuyuky <yasuyuki.ymd@gmail.com>
2019-10-05 22:55:09 +0900

    Remove example from test

--- src/lib.rs
+++ src/lib.rs
@@ -15,62 +15,12 @@ pub use cursormatrix::Term;
 #[cfg(test)]
 mod tests {
 
-    use crate::cursormatrix;
-    use crate::events::*;
-    use std::time::Duration;
+    use crate::Term;
     use term::terminfo::TermInfo;
 
     #[test]
-    fn test_term() -> Result<(), term::Error> {
-        fn handle_event(ev: &Event, term: &mut cursormatrix::Term) -> bool {
-            match ev {
-                &Event::Ctrl('C') => return false,
-                &Event::TimeOut => return false,
-                &Event::Ctrl('L') => term.cursor.clear().unwrap(),
-                &Event::Ctrl('R') => term.cursor.reload().unwrap(),
-                &Event::Ctrl('A') => term.cursor.move_home().unwrap(),
-                &Event::Ctrl('E') => term.cursor.move_end().unwrap(),
-                &Event::Arrow(Direction::Up) => term.cursor.move_up().unwrap(),
-                &Event::Arrow(Direction::Down) => term.cursor.move_down().unwrap(),
-                &Event::Arrow(Direction::Left) => term.cursor.move_left().unwrap(),
-                &Event::Arrow(Direction::Right) => term.cursor.move_right().unwrap(),
-                &Event::Ctrl('D') => term.cursor.delete_char().unwrap(),
-                &Event::Delete => term.cursor.delete_char().unwrap(),
-                &Event::BackSpace => term.cursor.backspace().unwrap(),
-                &Event::Chars(ref s) => {
-                    use unicode_normalization::UnicodeNormalization;
-                    term.cursor
-                        .print_here(&format!("{}", s.nfkc().collect::<String>()))
-                        .unwrap();
-                },
-                e => {
-                    let pos = term.cursor.get_pos();
-                    term.cursor
-                        .print_here(format!("e: {:?}, pos{:?}", e, pos).as_str())
-                        .unwrap();
-                },
-            }
-            true
-        }
-
-        let (mut term, erx) = cursormatrix::Term::with_input(Some(Duration::from_secs(10)), true)?;
-
-        term.cursor.print_fill_here("async!!!", 10).unwrap();
-        loop {
-            if match erx.recv() {
-                Ok(ev) => !handle_event(&ev, &mut term),
-                Err(_) => false,
-            } {
-                break;
-            }
-        }
-
-        Ok(assert!(true))
-    }
-
-    #[test]
     fn test_show_info() {
-        let term = match cursormatrix::Term::new() {
+        let term = match Term::new() {
             Ok(term) => term,
             Err(_) => return,
         };
