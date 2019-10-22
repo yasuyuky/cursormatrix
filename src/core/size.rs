a2430064c14e20dcf3badef4e88bedbcad515db0
yasuyuky <yasuyuki.ymd@gmail.com>
2019-10-23 08:46:57 +0900

    Refactor

--- src/core/size.rs
+++ src/core/size.rs
@@ -14,24 +14,25 @@ pub struct TermSize {
 #[allow(dead_code)]
 impl TermSize {
     pub fn from_tty(tty: &Tty) -> Result<TermSize, Error> {
-        let mut ws: libc::winsize = unsafe { mem::MaybeUninit::uninit().assume_init() };
-        let res = unsafe { libc::ioctl(tty.as_raw_fd(), libc::TIOCGWINSZ, &mut ws) };
-        if res != 0 {
-            return Err(Error::last_os_error());
-        }
+        let ws = Self::load_winsize(tty)?;
         Ok(TermSize { tty: tty.clone(),
                       width: ws.ws_col as usize,
                       height: ws.ws_row as usize })
     }
 
     pub fn refresh(&mut self) -> Result<(), Error> {
-        let mut ws: libc::winsize = unsafe { mem::MaybeUninit::uninit().assume_init() };
-        let res = unsafe { libc::ioctl(self.tty.as_raw_fd(), libc::TIOCGWINSZ, &mut ws) };
-        if res != 0 {
-            return Err(Error::last_os_error());
-        }
+        let ws = Self::load_winsize(&self.tty)?;
         self.width = ws.ws_col as usize;
         self.height = ws.ws_row as usize;
         Ok(())
+    }
+
+    fn load_winsize(tty: &Tty) -> Result<libc::winsize, Error> {
+        let mut ws: libc::winsize = unsafe { mem::MaybeUninit::uninit().assume_init() };
+        let res = unsafe { libc::ioctl(tty.as_raw_fd(), libc::TIOCGWINSZ, &mut ws) };
+        if res != 0 {
+            return Err(Error::last_os_error());
+        }
+        Ok(ws)
     }
 }
