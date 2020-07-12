d8c4efa457446d73d4ebee37acb7e09a41c8bb35
yasuyuky <yasuyuki.ymd@gmail.com>
2020-07-13 00:42:07 +0900

    Update example

--- examples/minieditor.rs
+++ examples/minieditor.rs
@@ -6,33 +6,29 @@ use std::time::Duration;
 fn handle_event(ev: &Event, term: &mut Term) -> bool {
     match ev {
         &Event::Ctrl('C') | &Event::TimeOut => return false,
-        &Event::Ctrl('L') => term.cursor.clear().unwrap(),
-        &Event::Ctrl('R') => term.cursor.reload().unwrap(),
-        &Event::Ctrl('A') => term.cursor.move_home().unwrap(),
-        &Event::Ctrl('E') => term.cursor.move_end().unwrap(),
-        &Event::Arrow(Direction::Up) => term.cursor.move_up().unwrap(),
-        &Event::Arrow(Direction::Down) => term.cursor.move_down().unwrap(),
-        &Event::Arrow(Direction::Left) => term.cursor.move_left().unwrap(),
-        &Event::Arrow(Direction::Right) => term.cursor.move_right().unwrap(),
+        &Event::Ctrl('L') => term.clear().unwrap(),
+        &Event::Ctrl('R') => term.reload().unwrap(),
+        &Event::Ctrl('A') => term.move_home().unwrap(),
+        &Event::Ctrl('E') => term.move_end().unwrap(),
+        &Event::Arrow(Direction::Up) => term.move_up().unwrap(),
+        &Event::Arrow(Direction::Down) => term.move_down().unwrap(),
+        &Event::Arrow(Direction::Left) => term.move_left().unwrap(),
+        &Event::Arrow(Direction::Right) => term.move_right().unwrap(),
         &Event::Ctrl('D') => term.cursor.delete_char().unwrap(),
         &Event::Return => {
-            term.cursor.print_here("↩︎").unwrap();
-            term.cursor.move_home().unwrap();
-            term.cursor.move_down().unwrap();
+            term.print_here("↩︎").unwrap();
+            term.move_home().unwrap();
+            term.move_down().unwrap();
         },
         &Event::Delete => term.cursor.backspace().unwrap(),
         &Event::BackSpace => term.cursor.backspace().unwrap(),
         &Event::Chars(ref s) => {
             use unicode_normalization::UnicodeNormalization;
-            term.cursor
-                .print_here(&format!("{}", s.nfkc().collect::<String>()))
-                .unwrap();
+            term.print_here(&format!("{}", s.nfkc().collect::<String>())).unwrap();
         },
         e => {
             let pos = term.cursor.get_pos();
-            term.cursor
-                .print_here(format!("e: {:?}, pos{:?}", e, pos).as_str())
-                .unwrap();
+            term.print_here(format!("e: {:?}, pos{:?}", e, pos).as_str()).unwrap();
         },
     }
     true
@@ -41,7 +37,7 @@ fn handle_event(ev: &Event, term: &mut Term) -> bool {
 fn main() {
     let (mut term, erx) = Term::with_input(Some(Duration::from_secs(10)), true).expect("term");
 
-    term.cursor.print_here("edit").unwrap();
+    term.print_here("edit").unwrap();
     loop {
         if match erx.recv() {
             Ok(ev) => !handle_event(&ev, &mut term),
