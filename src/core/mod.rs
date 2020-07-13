7471104fd977af4e4274b82aed8d210398640b06
yasuyuky <yasuyuki.ymd@gmail.com>
2020-07-14 08:33:11 +0900

    Remove size mod

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,7 +1,6 @@
 mod cursor;
 mod matrix;
 mod rune;
-mod size;
 mod terminfo;
 mod termios;
 mod tty;
@@ -9,7 +8,6 @@ mod tty;
 pub use crate::core::cursor::*;
 pub use crate::core::matrix::*;
 pub use crate::core::rune::*;
-pub use crate::core::size::*;
 pub use crate::core::terminfo::*;
 pub use crate::core::termios::*;
 pub use crate::core::tty::*;
